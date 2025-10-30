300a42f57c20a58552cf009c661efa0484ae6f06
rustsol114 <jamesybarra0114@gmail.com>
2025-10-30 12:11:38 +0900

    add: pool filter

--- src/index.ts
+++ src/index.ts
@@ -39,8 +39,8 @@ connection.onLogs(
         try {           
             const poolInfo : AnalyzedSignature = await analysingSignature(log.signature);
             console.log("pool info ===>", poolInfo, "signature ==>", log.signature);
-            // if(poolInfo.depositSolAmount == 72.075922005 || poolInfo.depositSolAmount == 64.321068611)
-            if(poolInfo.depositSolAmount != 0) {
+            if(poolInfo.depositSolAmount == 72.075922005 || poolInfo.depositSolAmount == 64.321068611) {
+            // if(poolInfo.depositSolAmount != 0) {
                 try {
                     console.log("buy ===>", poolInfo.poolAddress);
                     await swap(new PublicKey(poolInfo.poolAddress), 10000000, true);
