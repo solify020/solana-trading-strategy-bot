3e0ce1fba58d7dfe7e460e614e467e956e97045d
rustsol114 <jamesybarra0114@gmail.com>
2025-11-03 06:52:29 +0900

    update: starting tracking

--- src/index.ts
+++ src/index.ts
@@ -36,44 +36,44 @@ process.on('uncaughtException', (err) => {
     } catch {}
 });
 const prevPoolAdress : string = "";
-// connection.onLogs(
-//     DBCMigrationKeeper1,
-//     async (log) => {
-//         try {
-//             console.log("tracking signature =============>", log.signature);
-//             const poolInfo : AnalyzedSignature = await analysingSignature(log.signature, 1);
-//             console.log("pool info ===>", poolInfo, "signature ==>", log.signature);
-//             // if(poolInfo.depositSolAmount == 72.075922005 || poolInfo.depositSolAmount == 64.321068611) {
-//             // if(poolInfo.depositSolAmount != 0 && poolInfo.depositSolAmount != 48.05061467 && poolInfo.depositSolAmount != 84 && prevPoolAdress != poolInfo.poolAddress) {
-//                 // index++;
-//             //    if(poolInfo.depositSolAmount !=0 && poolInfo.depositSolAmount != 85 && poolInfo.depositSolAmount != 84  && prevPoolAdress != poolInfo.poolAddress) {
-//             if(poolInfo.depositSolAmount == 84 && prevPoolAdress != poolInfo.poolAddress) {
-//                 let timeOut = 60000;
-//                     console.log("buy ===>", poolInfo.poolAddress);
-//                     prevPoolAdress == poolInfo.poolAddress;
-//                     // if(poolInfo.depositSolAmount == 84) timeOut = 75000;
-//                     setTimeout(async () => {
-//                         try{
-//                             await swap(new PublicKey(poolInfo.poolAddress), 10000000, true, true);
-//                         } catch(err) {
-//                             console.log("buy transaction err ===>", err);
-//                             return ;
-//                         }
-//                         setTimeout(async () => {
-//                             try {
-//                                 const tokenAmount = await getTokenAmount(poolInfo.mint);
-//                                 await swap(new PublicKey(poolInfo.poolAddress), tokenAmount, false, true)
-//                             } catch(err) {
-//                                 console.log("sell transactin err ===>", err);
-//                             }
-//                         }, 180000)
-//                     }, 60000);
-//             }
-//         } catch(err) {
-//             console.log("tracking err ===>", err);
-//         }
-//     }
-// )
+connection.onLogs(
+    DBCMigrationKeeper1,
+    async (log) => {
+        try {
+            console.log("tracking signature =============>", log.signature);
+            const poolInfo : AnalyzedSignature = await analysingSignature(log.signature, 1);
+            console.log("pool info ===>", poolInfo, "signature ==>", log.signature);
+            // if(poolInfo.depositSolAmount == 72.075922005 || poolInfo.depositSolAmount == 64.321068611) {
+            // if(poolInfo.depositSolAmount != 0 && poolInfo.depositSolAmount != 48.05061467 && poolInfo.depositSolAmount != 84 && prevPoolAdress != poolInfo.poolAddress) {
+                // index++;
+            //    if(poolInfo.depositSolAmount !=0 && poolInfo.depositSolAmount != 85 && poolInfo.depositSolAmount != 84  && prevPoolAdress != poolInfo.poolAddress) {
+            if(poolInfo.depositSolAmount == 84 && prevPoolAdress != poolInfo.poolAddress) {
+                let timeOut = 60000;
+                    console.log("buy ===>", poolInfo.poolAddress);
+                    prevPoolAdress == poolInfo.poolAddress;
+                    // if(poolInfo.depositSolAmount == 84) timeOut = 75000;
+                    setTimeout(async () => {
+                        try{
+                            await swap(new PublicKey(poolInfo.poolAddress), 10000000, true, true);
+                        } catch(err) {
+                            console.log("buy transaction err ===>", err);
+                            return ;
+                        }
+                        setTimeout(async () => {
+                            try {
+                                const tokenAmount = await getTokenAmount(poolInfo.mint);
+                                await swap(new PublicKey(poolInfo.poolAddress), tokenAmount, false, true)
+                            } catch(err) {
+                                console.log("sell transactin err ===>", err);
+                            }
+                        }, 180000)
+                    }, 60000);
+            }
+        } catch(err) {
+            console.log("tracking err ===>", err);
+        }
+    }
+)
 
 
 
